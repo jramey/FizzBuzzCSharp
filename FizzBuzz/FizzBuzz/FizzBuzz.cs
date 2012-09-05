e0755911f10b568de4eb409babebfcc6ec2c68e1
Jordan Ramey <rameyj05@gmail.com>
2012-09-05 09:44:35 -0400

    Added FizzBuzz Project

---
+++ FizzBuzz/FizzBuzz/FizzBuzz.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Calculate
+{
+    public class FizzBuzz
+    {
+        public string shout(int i)
+        {
+            if (i % 3 == 0 && i % 5 == 0)
+                return "FizzBuzz";
+            else if (i % 5 == 0)
+                return "Buzz";
+            else if (i % 3 == 0)
+                return "Fizz";
+
+            return i.ToString();
+
+        }
+    }
+}
---
+++ FizzBuzz/TestProject1/FBTest.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Calculate;
+
+namespace TestProject1
+{
+    [TestClass]
+    public class FBTest
+    {
+        private FizzBuzz fizzbuzz;
+
+        public FBTest()
+        {
+            this.fizzbuzz = new FizzBuzz();
+        }
+
+        [TestMethod]
+        public void returnOne()
+        {
+            Assert.AreEqual("1", fizzbuzz.shout(1));
+        }
+
+        [TestMethod]
+        public void returnTwo()
+        {
+            Assert.AreEqual("2", fizzbuzz.shout(2));
+        }
+
+        [TestMethod]
+        public void returnFizzForThree()
+        {
+            Assert.AreEqual("Fizz", fizzbuzz.shout(3));
+        }
+
+        [TestMethod]
+        public void returnFour()
+        {
+            Assert.AreEqual("4", fizzbuzz.shout(4));
+        }
+
+        [TestMethod]
+        public void returnBuzzForFive()
+        {
+            Assert.AreEqual("Buzz", fizzbuzz.shout(5));
+        }
+
+        [TestMethod]
+        public void returnFizzForNine()
+        {
+            Assert.AreEqual("Fizz", fizzbuzz.shout(9));
+        }
+
+        [TestMethod]
+        public void returnBuzzForTen()
+        {
+            Assert.AreEqual("Buzz", fizzbuzz.shout(10));
+        }
+
+        [TestMethod]
+        public void returnFizzBuzzFor15()
+        {
+            Assert.AreEqual("FizzBuzz", fizzbuzz.shout(15));
+        }
+    }
+}
